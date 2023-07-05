8de696c8ff3bccfd7e4db2813f0a5168b32642e3
hrcn <hrcn1128@gmail.com>
2023-07-05 13:37:23 +0800

    init

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/rui-han/rss-aggregator
+
+go 1.20
+
+require (
+	github.com/go-chi/chi v1.5.4
+	github.com/joho/godotenv v1.5.1
+)
+
+require github.com/go-chi/cors v1.2.1
---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load()
+	portString := os.Getenv("PORT")
+
+	if portString == "" {
+		log.Fatal("PORT is not found")
+	}
+	fmt.Println("PORT: ", portString)
+
+	router := chi.NewRouter()
+
+	// cors configuration
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
+
+	v1Router := chi.NewRouter()
+	// only fire on GET requests
+	v1Router.Get("/healthz", HandlerReadiness)
+	v1Router.Get("/err", HandlerError)
+	router.Mount("/v1", v1Router)
+
+	server := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("Server is running on port: %v", portString)
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
